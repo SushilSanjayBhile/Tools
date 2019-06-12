cb60ebf70a1cb33662039fa12120f2d11f87d9df
Sushil <sushil@diamanti.com>
2019-06-12 11:01:41 +0530

    my bash script added. some docker commands with their flag is added.

---
+++ Bashrc/myBashScript.sh
@@ -0,0 +1,7 @@
+alias uuu="sudo apt update && sudo apt upgrade && sudo apt dist-upgrade"
+alias sshsush="gpg -d -q /home/sushil/.sshpasswd.gpg > fifo; sshpass -f fifo ssh sushil@testserver3"
+alias vd="sudo vpnc-disconnet"
+alias vc="sudo vpnc-connect"
+alias .b=". ~/.bashrc"
+
+cat ~/Documents/Study/Git/Tools/Bashrc/myBashScript.sh
