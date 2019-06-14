baa068906bb124949d8677e7ff3b43b1387c4dfd
Sushil <sushil@diamanti.com>
2019-06-14 16:44:45 +0530

    bash command inside another command. Previously decrypting gpg key in a file and passing that file to sshpass. but decrypted password was kept in readable format. So did this.

--- Bashrc/myBashScript.sh
+++ Bashrc/myBashScript.sh
@@ -1,6 +1,6 @@
 alias uuu="sudo apt update && sudo apt upgrade && sudo apt dist-upgrade"
-alias sshsush="gpg -d -q /home/sushil/.sshpasswd.gpg > fifo; sshpass -f fifo ssh sushil@testserver3"
-alias vd="sudo vpnc-disconnet"
+alias sshsush="sshpass -p \`gpg -d -q /home/sushil/.sshpasswd.gpg\` ssh sushil@testserver3"
+alias vd="sudo vpnc-disconnect"
 alias vc="sudo vpnc-connect"
 alias .b=". ~/.bashrc"
 
